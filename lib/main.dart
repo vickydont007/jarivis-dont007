c0c1a0f04ab55e9bd208745717d1cb747520141a
Vicky Dont <abc@ABCs-MacBook-Pro.local>
2026-06-11 23:10:34 +0530

    fix: Remove permission_handler causing black screen
    - Removed permission_handler from main.dart
    - Speech_to_text handles permission internally
    - Build successful

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:window_manager/window_manager.dart';
-import 'package:permission_handler/permission_handler.dart';
 import 'app.dart';
 
 void main() async {
@@ -9,9 +8,6 @@ void main() async {
 
   // Initialize window manager
   await windowManager.ensureInitialized();
-
-  // Request microphone permission
-  await [Permission.microphone].request();
 
   const windowOptions = WindowOptions(
     size: Size(1200, 800),
